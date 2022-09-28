0dc6c97bb3b3b3c17749c88abb7cee3dc4b01349
raisadiakbar <muhrais791@gmail.com>
2022-09-28 18:16:19 +0900

    +

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,6 +1,5 @@
 const db = require('../models')
 const Orders = db.Orders;
-const Customers = db.Customers;
 const Op = db.Sequelize.Op;
 const controller = {};
 
@@ -21,20 +20,13 @@ controller.getAll = async (req, res) => {
 }
 
 controller.addOrder = async (req, res, next) => {
-    const {customer_id, item_id, qty, status, payment_method} = req.body;
-        const orderItem = await Items.findOne({
-            where: {
-                 id: req.body.item_id
-            }
-        });
-          
+    const { qty, status, payment_method, confirm_payment } = req.body;
         const order = {
-            customer_id   : customer_id,
-            item_id       : item_id,
             qty           : qty,
             amount        : qty * orderItem.dataValues.price,
             status        : status,
             payment_method: payment_method,
+            confirm_payment: confirm_payment
         }
 
         await Orders.create(order)
@@ -69,12 +61,11 @@ controller.updateOrder = async (req, res, next) => {
             }
         })
         const order = {
-            customer_id   : req.body.customer_id,
-            item_id       : req.body.item_id,
             qty           : req.body.qty,
             amount        : req.body.qty * orderItem.dataValues.price,
             status        : req.body.status,
             payment_method: req.body.payment_method,
+            confirm_payment: req.body.confirm_payment
         }
          await Orders.update(order,{
              where: {
