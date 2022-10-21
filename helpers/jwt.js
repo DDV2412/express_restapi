0a77ccd4d5c3f699a7896b8261094181ef978a8f
DDV2412 <dhyanputra24@gmail.com>
2022-10-21 21:56:33 +0700

    ✅ Revisi Auth Unit Test #2

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,9 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 const encode = (data) =>
-  jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1h" });
+  jwt.sign(
+    data,
+    process.env.JWT_SECRET ||
+      "QWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2N",
+    { expiresIn: "1h" }
+  );
 
-const decode = (token) => jwt.verify(token, process.env.JWT_SECRET);
+const decode = (token) =>
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET ||
+      "QWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2N"
+  );
 
 module.exports = {
   encode,
